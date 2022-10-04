755b6ccb878e4b03acf2839337739fa017bcf5c1
JayT <trawickj@gmail.com>
2022-10-04 13:27:14 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jay";
+    int age = 31;
+    String state = "North Carolina";
+
+    System.out.println("Name: " + name + "Age: " + age + "State: " + state);
+  }
+}
