1ec106f1c6a3ad8995f3c09a170dea292c47e656
JayT <trawickj@gmail.com>
2022-10-04 13:38:11 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year;
+    String month;
+    boolean weekend;
+    year = 1991;
+    month = "October";
+    weekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
