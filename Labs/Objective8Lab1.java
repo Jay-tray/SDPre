7850a589ae93148148faca5fc07f995476e1140e
JayT <trawickj@gmail.com>
2022-10-06 10:55:07 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int num =0; num <=10; num++){
+      System.out.println(num);
+    }
+    for (int num =10; num >=0; num-=1){
+      System.out.println(num);
+    }
+  }
+}
